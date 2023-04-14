7b1b9a54071104188fc58b841b0c954e481c00ef
Terhi-R <112477395+Terhi-R@users.noreply.github.com>
2023-04-14 14:42:54 +0200

    DbContext and Controllers set up

---
+++ browseEasy.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using browseEasy.API.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<browseEasy.API.Models.User> User { get; set; }
+
+        public DbSet<browseEasy.API.Models.Group> Group { get; set; }
+    }
