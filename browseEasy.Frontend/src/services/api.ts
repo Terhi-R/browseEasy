f3d53355c1d53c6e478972801b76d5e32bd4f96e
Terhi-R <112477395+Terhi-R@users.noreply.github.com>
2023-04-20 10:09:55 +0200

    apis and interfaces set

---
+++ browseEasy.Frontend/src/services/api.ts
@@ -0,0 +1,57 @@
+import { IUser } from "./interfaces";
+
+export const getUsers = async () => {
+  const users: IUser[] = await fetch(
+    "https://browseeasyapi.azurewebsites.net/api/Users"
+  )
+    .then((response) => response.json())
+    .then((data) => data);
+  return users;
+};
+
+export const getUser = async (id: number) => {
+  const user: IUser[] = await fetch(
+    `https://browseeasyapi.azurewebsites.net/api/Users/${id}`
+  )
+    .then((response) => response.json())
+    .then((data) => data);
+  return user;
+};
+
+export const postUsers = async (newUser: Partial<IUser>) => {
+  const user: IUser[] = await fetch(
+    `https://browseeasyapi.azurewebsites.net/api/Users`, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+        "content-type": "application/json",
+        }
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => data);
+  return user;
+};
+
+export const putUsers = async (id: number, editedUser: Partial<IUser>) => {
+  const user: IUser[] = await fetch(
+    `https://browseeasyapi.azurewebsites.net/api/Users/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(editedUser),
+        headers: {
+        "content-type": "application/json",
+        }
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => data);
+  return user;
+};
+
+export const deleteUsers = async (id: number) => {
+  await fetch(
+    `https://browseeasyapi.azurewebsites.net/api/Users/${id}`, {
+        method: "DELETE"
+    }
+  )
+};
---
+++ browseEasy.Frontend/src/services/interfaces.ts
@@ -0,0 +1,29 @@
+export interface IUser {
+    id: number,
+    name: string,
+    platforms: IPlatform[],
+    type: string,
+    IMDbRating: number,
+    genres: IGenre[],
+    groups: IGroup[],
+    movies: IMovie[]
+}
+
+export interface IPlatform {
+    id: number,
+    name: string
+}
+
+export interface IGenre {
+    id: number,
+    name: string
+}
+export interface IMovie {
+    id: number,
+    name: string
+}
+export interface IGroup {
+    id: number,
+    name: string,
+    uniqueKey: string
+}
