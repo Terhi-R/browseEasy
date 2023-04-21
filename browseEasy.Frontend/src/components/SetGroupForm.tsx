0932ffdb150d37f291cf80ab5e19c9f87b05661d
Terhi-R <112477395+Terhi-R@users.noreply.github.com>
2023-04-21 14:16:12 +0200

    Group form complete

--- browseEasy.Frontend/src/components/SetGroupForm.tsx
+++ browseEasy.Frontend/src/components/SetGroupForm.tsx
@@ -1,4 +1,4 @@
-import { FC, SyntheticEvent, useContext, useEffect, useState } from "react"
+import { ChangeEvent, FC, FormEvent, SyntheticEvent, useContext, useEffect, useRef, useState } from "react"
 import "../stylesheets/SetGroupForm.css"
 import firebase from 'firebase/compat/app';
 import { GroupContext, UserContext } from "../App";
@@ -10,77 +10,61 @@ type SetGroupFormProps = {
     openForm: () => void
 }
 
-const initialValues = {
-  userName: "",
-  groupName: "",
-  newUniqueKey: "",
-  foundGroupKey: "",
-  uniqueKey: "",
-  foundGroup: false,
-};
-
 export const SetGroupForm: FC<SetGroupFormProps> = ({openForm}) => {
     const users = useContext(UserContext);
     const groups = useContext(GroupContext);
     const activeUser = useContext(ActiveUserContext);
-    const [values, setValues] = useState(initialValues);
+    const [matchingGroup, setMatchingGroup] = useState(false);
     
     const preferencesPage = () => {
         openForm();
     }
 
-    const handleChange = (e: SyntheticEvent) => {
-        const target = e.target as typeof e.target & {
-        userName: { value: string };
-        groupName: { value: string };
-        newUniqueKey: { value: string | undefined };
-        uniqueKey: { value: string | undefined };
-        };
-
-        if (target.groupName !== undefined && target.groupName.value.length > 0) {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const groupName = e.currentTarget.value;
+         if (groupName !== undefined && groupName.length > 0) {
             groups.map(group => {
-                if (group.name == target.groupName.value) {
-                setValues({...values, foundGroup: true, foundGroupKey: group.uniqueKey!})
+                if (group.name == groupName) {
+                setMatchingGroup(true);
                 } else {
-                    setValues({...values, foundGroup: false})
+                    setMatchingGroup(false);
                 }});
-            console.log(target.groupName.value);
-            setValues({
-            ...values,
-            userName: target.userName.value,
-            groupName: target.groupName.value,
-            newUniqueKey: target.newUniqueKey.value === undefined ? "" : target.newUniqueKey.value,
-            uniqueKey: target.uniqueKey.value === undefined ? "" : target.uniqueKey.value,
-            });
-        }
-
+        } 
     }
 
-    const handleSubmit = (e: SyntheticEvent) => {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (initialValues.foundGroup) {
+        const target = e.currentTarget.elements as typeof e.currentTarget.elements & {
+        userName: { value: string };
+        groupName: { value: string };
+        uniqueKey: { value: string };
+        };
+
+        if (matchingGroup) {
             console.log("found Group");
             groups.map(group => {
-                if (group.uniqueKey == initialValues.uniqueKey && initialValues.uniqueKey == initialValues.foundGroupKey) {
+                if (group.uniqueKey == target.uniqueKey.value) {
                     users.map(user => {
                         if (user.loginId === activeUser.id) {
+                            user.name = target.userName.value;
                             user.groups.push(group);
-                            console.log(user.groups);
                             addGroupToUser(user.id, user);
                         }
                     })
                 }})
-            preferencesPage();
-            return;
-        }
-        users.map(user => {
-            if (user.loginId === activeUser.id) {
+                preferencesPage();
+                return;
+            }
+            users.map(user => {
+                if (user.loginId === activeUser.id) {
                 const newGroup : IGroup = {
-                    name: "test",
-                    uniqueKey: "testpassword"
+                    name: target.groupName.value,
+                    uniqueKey: target.uniqueKey.value
                 }
+                user.name = target.userName.value;
                 user.groups.push(newGroup);
-                console.log(user.groups);
+                 console.log(user.groups);
+                console.log(newGroup); 
                 addGroupToUser(user.id, user);
             }
         })
@@ -98,19 +82,19 @@ export const SetGroupForm: FC<SetGroupFormProps> = ({openForm}) => {
         <h2>Hi {activeUser.name}! Let's get you set up.</h2>
         <form className="group-form" onSubmit={handleSubmit}>
             <label>Your name</label>
-            <input type="text" name="userName" onChange={handleChange} value={activeUser.name}></input>
+            <input type="text" name="userName" defaultValue={activeUser.name}></input>
             <label>Your group name:</label>
             <input type="text" name="groupName" onChange={handleChange}></input>
-            {initialValues.foundGroup && 
+            {matchingGroup && 
             <>
             <label>We already found one! Do you have the unique key?</label>
-            <input type="text" name="uniqueKey" onChange={handleChange}></input>
+            <input type="text" name="uniqueKey"></input>
             </>
             }
-            {!initialValues.foundGroup && 
+            {!matchingGroup && 
             <>
             <label>Create unique group keyword:</label>
-            <input type="text" name="newUniqueKey" onChange={handleChange}></input>
+            <input type="text" name="uniqueKey"></input>
             </>
             }
             <button type="submit">Go to preferences</button>
